916c75ea86d7f70e7d1eeb20371431995a472181
cappors@gmail.com <konjuktivitis84>
2017-12-12 21:17:44 +0100

    Server complete setup and star working on frontend

---
+++ client/commponents/LoginForm.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+
+
+
+class LoginForm extends Component {
+
+    render(){
+        return (
+            <div>Login Form</div>
+        )
+    }
+
+}
+
+export default LoginForm;
--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -6,20 +6,39 @@ const {
 
 
 const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
 
 const mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
-       signup : {
-           type: UserType,
-           args: {
-               email: {type: GraphQLString},
-               password: {type: GraphQLString}
-           },
-           resolve(parentValue, args, request){
-
-           }
-       }
+        signup: {
+            type: UserType,
+            args: {
+                email: {type: GraphQLString},
+                password: {type: GraphQLString}
+            },
+            resolve(parentValue, {email, password}, req){
+                return AuthService.signup({email, password, req})
+            }
+        },
+        login: {
+            type: UserType,
+            args: {
+                email: {type: GraphQLString},
+                password: {type: GraphQLString}
+            },
+            resolve(parentValue, {email, password}, req){
+                return AuthService.login({email, password, req})
+            }
+        },
+        logout: {
+            type: UserType,
+            resolve(parentValue, args, req){
+                const {user} = req;
+                req.logout();
+                return user;
+            }
+        }
     }
 });
 
